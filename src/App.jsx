9109b751d1bad57f8bf499822559af229f6c43d0
nikkelliaa <niyaraemelbekova242@gmail.com>
2024-05-29 17:09:35 +0300

    made routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 
 import './App.css';
+import Header from "./components/header/Header";
+import Payment from "./pages/ payment/ Payment";
+import AboutUs from "./pages/aboutUs/AboutUs";
+import Banal from "./pages/banal/Banal";
+import Contact from "./pages/contact/Contact";
+import HomePage from "./pages/home/HomePage";
+import InStock from "./pages/inStock/inStock";
+import {Route, Routes} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+      <div className="App">
+          <Header />
+          <Routes>
+              <Route exact path="/" element={<HomePage/>}/>
+              <Route path="header" element={<Header/>} />
+              <Route path="payment" element={<Payment/>}/>
+              <Route path="aboutUs" element={<AboutUs/>}/>
+              <Route path="banal" element={<Banal/>}/>
+              <Route path="contact" element={<Contact/>}/>
+              <Route path="inStock" element={<InStock/>}/>
+          </Routes>
+      </div>
   );
 }
 
 export default App;
+
+
+
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import {Outlet, Link} from 'react-router-dom'
 
 const Header = () => {
     return (
-        <div>
-            Header
-        </div>
+        <>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/aboutUs">AboutUs</Link>
+                    </li>
+                    <li>
+                        <Link to="/payment">Payment</Link>
+                    </li>
+                    <li>
+                        <Link to="/Banal">Banal</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                    <li>
+                        <Link to="/inStock">InStock</Link>
+                    </li>
+                    <li>
+                        <Link to="/product">Product</Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <Outlet/>
+        </>
     );
 };
 
